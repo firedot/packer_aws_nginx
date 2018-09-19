cb3aeff65a1bf0362451a9d62a6806dee3617599
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-09-19 10:23:05 +0300

    Edited multiple files

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,6 +1,6 @@
-apt-get clean
-apt-get update
-apt-get install -y nginx
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get install -y nginx
 # Hide Ubuntu splash screen during OS Boot, so you can see if the boot hangs
 #apt-get remove -y plymouth-theme-ubuntu-text
 #sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
